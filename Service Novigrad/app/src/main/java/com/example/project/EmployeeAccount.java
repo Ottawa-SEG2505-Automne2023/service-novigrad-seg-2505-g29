e5dfc7796c18d4e9093356e354b4237212a30e81
ODaal <117872338+ODaal@users.noreply.github.com>
2023-11-25 22:33:48 -0500

    Livrable3 Samedi

--- Service Novigrad/app/src/main/java/com/example/project/EmployeeAccount.java
+++ Service Novigrad/app/src/main/java/com/example/project/EmployeeAccount.java
@@ -1,7 +1,11 @@
 package com.example.project;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeeAccount extends Account{
 
+    List<Service> OfferedServices;
     public EmployeeAccount(){
     }
     public EmployeeAccount(String firstName, String lastName, String email, String password) {
@@ -9,5 +13,6 @@ public class EmployeeAccount extends Account{
         this.lastName = lastName;
         this.email = email;
         this.password = password;
+        OfferedServices = new ArrayList<>();
     }
 }
