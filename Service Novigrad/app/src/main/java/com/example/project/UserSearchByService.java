9b1962b930f2dad6b157ef56354af419c89cafc2
petiteboi <gabriel.pilon01@gmail.com>
2023-12-07 00:52:08 -0500

    All search functions exist but they all crash and i have no idea what to do i hate androidstudio please get me out of here

--- Service Novigrad/app/src/main/java/com/example/project/UserSearchByService.java
+++ Service Novigrad/app/src/main/java/com/example/project/UserSearchByService.java
@@ -2,7 +2,15 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UserSearchByService extends AppCompatActivity {
 
@@ -12,6 +20,30 @@ public class UserSearchByService extends AppCompatActivity {
         setContentView(R.layout.activity_user_search_by_service);
 
         //Variables
+        EditText searchedService = findViewById(R.id.txtSearchField);
+        Button btnSearch = findViewById(R.id.btnSearch);
+        Button btnBack = findViewById(R.id.btnBack);
 
+        String serviceQuery = searchedService.getText().toString();
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+        //search for the service in the search field
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent = new Intent(UserSearchByService.this, UserServiceSearchResultActivity.class);
+                    intent.putExtra("service", serviceQuery);// bring the service being searched for to the next activity
+                    startActivity(intent);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
